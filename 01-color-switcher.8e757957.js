f414fd7005aa50ff68c5f02765959f26415973ac
Alejandro111555 <47757338+Alejandro111555@users.noreply.github.com>
2022-06-20 20:54:33 +0000

    Deploying to gh-pages from @ Alejandro111555/goit-js-hw-09@f60007e600f6a337d83f44b270021644c25090ee 🚀

---
+++ 01-color-switcher.8e757957.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");o.setAttribute("disabled",!0),e.addEventListener("click",(()=>{color=setInterval((()=>{!function(){const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}()}),1e3),e.setAttribute("disabled",!0),o.removeAttribute("disabled")})),o.addEventListener("click",(()=>{clearInterval(colo),e.removeAttribute("disabled"),o.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.8e757957.js.map
